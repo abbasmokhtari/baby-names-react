1395bf19ca54463771fb81827b2e7619d69a1e60
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2020-06-08 12:36:17 +0100

    first week done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NameBlock from './NameBlock';
+import babyNameData from './babyNameData.json'
+
 
 function App() {
   return (
     <div className="App">
+      <NameBlock allName={babyNameData} />
     </div>
   );
 }
